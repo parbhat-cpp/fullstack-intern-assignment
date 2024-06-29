c218bf3d39b7e2a92857947c162ac21702af53dd
Parbhat Sharma <prabhat29ps@gmail.com>
2024-06-29 12:06:18 +0530

    useAuthSession implementation

--- hooks/useAuthSession.ts
+++ hooks/useAuthSession.ts
@@ -1,12 +1,39 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setUser, clearAuth } from "@/redux/auth/auth.slice";
+import { setUser, clearAuth, setToken } from "@/redux/auth/auth.slice";
 import { RootState } from "@/redux/store";
+import { toast } from "react-toastify";
 
 const useAuthSession = () => {
   const dispatch = useDispatch();
   const user = useSelector((state: RootState) => state.auth.user);
-  //  implement the logic here to check user session
+  const token = useSelector((state: RootState) => state.auth.token);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      // check if token exists
+      if (token) {
+        const authResponse = await fetch('/api/session', {
+          headers: {
+            token
+          }
+        });
+
+        // on successful authentication
+        if (authResponse.ok) {
+          const {username} = await authResponse.json();
+          dispatch(setUser({username}));
+        }
+      } 
+      // on authentication fail
+      else {
+        toast.info("User not authenticated");
+      }
+    }
+
+    checkAuth();
+  }, [token]);
+
   return user;
 };
 
