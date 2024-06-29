d2b5b21d15a04c134b3842f08c5502bc0d4efbf7
Parbhat Sharma <prabhat29ps@gmail.com>
2024-06-29 12:06:33 +0530

    sample user login data

---
+++ users/index.ts
@@ -0,0 +1,27 @@
+export const users = [
+    {
+        id: 1,
+        username: 'user1',
+        password: '12345678'
+    },
+    {
+        id: 1,
+        username: 'user2',
+        password: '12345678'
+    },
+    {
+        id: 1,
+        username: 'user3',
+        password: '12345678'
+    },
+    {
+        id: 1,
+        username: 'user4',
+        password: '12345678'
+    },
+    {
+        id: 1,
+        username: 'user5',
+        password: '12345678'
+    },
+]
